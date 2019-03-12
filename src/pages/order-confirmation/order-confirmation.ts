bce094790b8b2ba4df4b63f6455898baa66f091a
Marshall Paiva <marshallpaiva@hotmail.com>
2019-03-12 14:53:26 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -1,3 +1,4 @@
+import { PedidoService } from './../../services/domain/pedido.service';
 import { ClienteService } from './../../services/domain/cliente.service';
 import { EnderecoDTO } from './../../models/endereco.dto';
 import { ClienteDTO } from './../../models/cliente.dto';
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
 
     this.pedido = this.navParams.get('pedido');
 
@@ -50,4 +52,20 @@ export class OrderConfirmationPage {
     return this.cartService.total();
   }
 
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+    .subscribe(response => {
+      this.cartService.createOrClearCart();
+      console.log(response.headers.get('location'));
+    },
+    error => {
+      if (error.status == 403) {
+        this.navCtrl.setRoot('HomePage');
+      }
+    })
+  }
+
+  back() {
+    this.navCtrl.setRoot('CartPage');
+  }
 }
